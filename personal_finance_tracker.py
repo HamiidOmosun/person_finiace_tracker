3a84fd58720f2ea2a4df559aafe9a573f83fc5b3
HamiidOmosun <Abdulhamiidomosun@gmail.com>
2025-07-21 10:52:32 +0100

    modified:   personal_finance_tracker.py

--- personal_finance_tracker.py
+++ personal_finance_tracker.py
@@ -8,31 +8,28 @@ class Transcations:
     self.type = type_of_transaction
     self.category = category
     self.date = datetime.now()
-    
+
+class FinanceManager:
+  #this keeps track of all action transcations and manager the finance
+  def __init__(self):
+    pass
+  
   def total_income(self):
     pass
   
   def total_expenses(self):
     pass
   
-  def balance(self):
+  def check_balance(self):
     pass
-
-class FinanceManager:
-  #this keeps track of all action transcations and manager the finance
-  def __init__(self):
-    pass
-    
-  def user_input(self):
+  
+  def get_transaction(self):
     pass
   
   def add_income(self):
     pass
   
   def add_expenses(self):
-    pass
-  
-  def add_to_category(self):
     pass
   
   def show_summary(self):
@@ -53,6 +50,9 @@ class FileHandler:
 class CliController:
   #this handles how to the user interracters with the cli
   def __init__(self):
+    pass
+  
+  def user_input(self):
     pass
   
   def menu(self):
